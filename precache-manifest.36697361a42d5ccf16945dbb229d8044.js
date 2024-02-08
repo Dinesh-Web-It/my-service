04486dbf1376bfc27533091317292d494d1aa8e3
Dinesh K <152156088+Dinesh-Web-It@users.noreply.github.com>
2024-02-08 07:09:36 +0530

    Updates

---
+++ precache-manifest.36697361a42d5ccf16945dbb229d8044.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d3feb235d2a851b9403e120b9e39a4",
+    "url": "/my-service/index.html"
+  },
+  {
+    "revision": "c30adbe101c544228e20",
+    "url": "/my-service/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "8d5545f6b1c44bfadf17",
+    "url": "/my-service/static/css/main.dfc82bff.chunk.css"
+  },
+  {
+    "revision": "c30adbe101c544228e20",
+    "url": "/my-service/static/js/2.354bdeaa.chunk.js"
+  },
+  {
+    "revision": "c4a92d83bff9d017a1c3",
+    "url": "/my-service/static/js/3.d637a5b4.chunk.js"
+  },
+  {
+    "revision": "8d5545f6b1c44bfadf17",
+    "url": "/my-service/static/js/main.4d2c6463.chunk.js"
+  },
+  {
+    "revision": "d69b179291911b108819",
+    "url": "/my-service/static/js/runtime~main.2338252e.js"
+  },
+  {
+    "revision": "5e2f64d27b5150ab3dafdace180fb199",
+    "url": "/my-service/static/media/3d-casual-life-woman-holding-phone-with-white-screen.5e2f64d2.png"
+  },
+  {
+    "revision": "f30dcc3b6656bb81f5edd134476643ee",
+    "url": "/my-service/static/media/Home screen-rafiki.f30dcc3b.png"
+  },
+  {
+    "revision": "33b28176c3ae6e8d5ed8088571b9d9e5",
+    "url": "/my-service/static/media/apple1.33b28176.png"
+  },
+  {
+    "revision": "8f9a3439c65d40597e9a1c9b821f2f21",
+    "url": "/my-service/static/media/battery-replacement.8f9a3439.png"
+  },
+  {
+    "revision": "f1125605a6b0da00a599e31fcd9657da",
+    "url": "/my-service/static/media/email.f1125605.png"
+  },
+  {
+    "revision": "58b621fefa45c6700f7ae4652bc8cf1b",
+    "url": "/my-service/static/media/google.58b621fe.jpg"
+  },
+  {
+    "revision": "ad9fd5eae8ebb678c71f64c89759561a",
+    "url": "/my-service/static/media/instagram.ad9fd5ea.png"
+  },
+  {
+    "revision": "e1b6edb9e4fcec63335e8d29026ecea3",
+    "url": "/my-service/static/media/logo1.e1b6edb9.png"
+  },
+  {
+    "revision": "3488755a18ebaa7f44df4ba2dd3d1524",
+    "url": "/my-service/static/media/motherboard-repair.3488755a.png"
+  },
+  {
+    "revision": "1bb97d3269be90cb1a95b3a85f8af396",
+    "url": "/my-service/static/media/profile3d.1bb97d32.png"
+  },
+  {
+    "revision": "23f7c3b6c7048693ba752cecb81e2a56",
+    "url": "/my-service/static/media/repair.23f7c3b6.png"
+  },
+  {
+    "revision": "35dbbf8508761cd36fa1374a10c7a217",
+    "url": "/my-service/static/media/software update.35dbbf85.jpg"
+  },
+  {
+    "revision": "c228a51f52d262f60b78c320bb8fb782",
+    "url": "/my-service/static/media/twitter.c228a51f.png"
+  },
+  {
+    "revision": "67e5d5d8d857437ba1ea653ddf311cb9",
+    "url": "/my-service/static/media/water-damage-repair.67e5d5d8.png"
+  },
+  {
+    "revision": "2e4ba20fd1e52dae681b1d6736588219",
+    "url": "/my-service/static/media/whatsapp (1).2e4ba20f.png"
+  }
+]);
